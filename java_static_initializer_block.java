47695f4c8035ffbe63d571212f681e9355767408
Bhavya Singh <bhavyarowdy@gmail.com>
2016-12-22 23:49:48 +0530

    Java static initializer block
    You are given a class Solution with a main method. Complete the given
    code so that it outputs the area of a parallelogram with breadth B and
    height H . You should read the variables from the standard input.
    
    If B<0or H<0 , the output should be "java.lang.Exception: Breadth and
    height must be positive" without quotes.

---
+++ java_static_initializer_block.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+static Scanner sc=new Scanner(System.in);
+static int B=sc.nextInt();
+static int H=sc.nextInt();
+static boolean flag=true;
+static{try{
+    if(B<=0 || H<=0){
+        flag=false;
+    System.out.print("java.lang.Exception: Breadth and height must be positive");
+    }
+}
+catch(Exception area){
+    System.out.println(area);
+}
+      }
+      public static void main(String[] args){
+		if(flag){
+			int area=B*H;
+			System.out.print(area);
+		}
+
+	}//end of main
+
+}//end of class
